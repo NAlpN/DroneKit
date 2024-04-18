1c47432de0d44c6837f5e39a61a382e320e946e5
Alperen <138169703+NAlpN@users.noreply.github.com>
2024-04-18 21:08:09 +0300

    Hava aracının uçuş açısının değiştirilmesi için

---
+++ ANGLE.py
@@ -0,0 +1,18 @@
+from dronekit import connect, VehicleMode,LocationGlobalRelative
+import time
+from pymavlink import mavutil
+
+connection_string="127.0.0.1:14550"
+
+vehicle = connect(connection_string,wait_ready=True,timeout=100)
+
+vehicle.armed = True
+
+vehicle.mode = VehicleMode('TAKEOFF')
+print("TAKEOFF moduna geçildi.")
+
+vehicle.simple_takeoff(20)
+
+msg = vehicle.message_factory.command_long_encode(0, 0,mavutil.mavlink.MAV_CMD_CONDITION_YAW,0,100,0,1,0,0, 0, 0)
+vehicle.send_mavlink(msg)
+print("Açı değiştirildi.")
